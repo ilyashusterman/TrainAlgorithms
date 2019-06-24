10c0885967a556c51b425d9305acfa65a91a7435
Ilya Shusterman <shusterilyaman@gmail.com>
2019-06-24 17:05:52 +0300

    add: exersice print

--- TwoSum.h
+++ TwoSum.h
@@ -9,18 +9,42 @@
 #include <iostream>
 #include <stdio.h>
 
+/*
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ */
+
 class TwoSum {
 
     public: int main() {
         std::cout << "Running, TwoSum" << std::endl;
         std::vector<int> array = { 1, 2,3,4};
-        printArrayInt(this->twoSum(array, 7), array);
+        int target = 7;
+        calculateAndPrint(array, target);
+        array = { 2,7,11,15, 16, 1, 2, 3, 5, 7, 9};
+        target = 8;
+        calculateAndPrint(array, target);
+        array = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+        target=2;
+        calculateAndPrint(array, target);
         return 0;
     }
 
-    public: std::vector<int> twoSum(std::vector<int> nums, int target) {
-            int tempNumber = 0;
-            int secondTempNumber = 0;
+    void calculateAndPrint(const std::vector<int> &array, int target) {
+        std::vector<int> sum_indexes = twoSum(array, target);
+        printArrayInt(sum_indexes, array);
+        printTarget(target);
+    }
+
+public: std::vector<int> twoSum(std::vector<int> nums, int target) {
+        int tempNumber = 0;
+        int secondTempNumber = 0;
             for(int i = 0; i < nums.size(); ++i) {
                 tempNumber = nums[i];
                 if (tempNumber <= target){
@@ -33,7 +57,7 @@ class TwoSum {
                 }
             }
 
-        }
+    }
 
     void printArrayInt(std::vector<int>  indexes, std::vector<int>  array) const {
         std::cout << "The indexes of array are:";
@@ -41,6 +65,15 @@ class TwoSum {
              it != indexes.end(); ++it) {
              printf ("array[%d]=%d ", *it,  array[*it]);
         }
+        printf (" [");
+        for (std::vector<int>::iterator it = array.begin(); it != array.end(); ++it){
+            printf ("%d,", *it);
+        }
+        printf ("]");
+    }
+
+    void printTarget(int target) const {
+        printf(" for target %d", target);
         std::cout << '\n';
     }
 };
